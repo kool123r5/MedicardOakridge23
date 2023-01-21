b667246900088351c29dc26456877c71c39c28e9
Ashwin <79413961+dev-ashwin7@users.noreply.github.com>
2023-01-22 01:12:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ import cv2
 import requests
 
 
-
+#todo
+# ocr with camera
+# navigation buttons
+# styling
+# buttons to shwinn + ando work
 
 
 currentEmail = ''
@@ -26,13 +30,14 @@ class Window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("MediCard")
-        self.geometry("390x844")
+        self.geometry("390x644")
         self.resizable(False, False)
 
         self.pfpImage = tk.PhotoImage(file = "pfp.png")
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
+
 
 
         self.homepage = Homepage(self)
@@ -60,7 +65,8 @@ class Maincard(ctk.CTkFrame):
     fg='#ffffff',
     bd=0,
     font=('calibri', 20, 'bold'))
-        self.basic_info_label.pack()
+        self.basic_info_label.pack(fill = "x")
+
 
         
         
@@ -99,10 +105,12 @@ class Maincard(ctk.CTkFrame):
 
             self.OCRBtn = ctk.CTkButton(self, text = "Scan Document", command = self.OCRCard)
             self.OCRBtn.pack()
+
+            self.goback = ctk.CTkButton(self, text = "Go Back", command = self.goback)
+            self.goback.pack()
             
 
 
-        
 
     def editCard(self):
         self.container.editcard.tkraise()
@@ -111,7 +119,7 @@ class Maincard(ctk.CTkFrame):
     
     def goback(self):
         self.container.homepage.showcard()
-        self.container.homepage.showcard.tkraise()
+        self.container.homepage.tkraise()
         
 
 class Editcard(ctk.CTkFrame):
@@ -150,8 +158,6 @@ class Homepage(ctk.CTkFrame):
         self.container =  container
         self.pfpImage = self.container.pfpImage
         self.count = 0
-        self.pfpBtn = tk.Button(self, bg = "white", border = 0, width = 100, height = 100, image = self.pfpImage)
-        self.pfpBtn.place(x = 10, y = 10)
         self.company_name = ctk.CTkLabel(self, text= "MEDICARD")
         self.company_name.pack()
         self.li = []
@@ -160,7 +166,9 @@ class Homepage(ctk.CTkFrame):
     fg='#ffffff',
     bd=0,
     font=('calibri', 20, 'bold'), command = self.showmaincard)
-        self.cardBtn.pack()
+        self.cardBtn.pack(fill = "x")
+        self.pfpBtn = tk.Button(self, bg = "white", border = 0, width = 70, height = 70, image = self.pfpImage)
+        self.pfpBtn.place(x = 10, y = 50)
 
         
 
@@ -240,10 +248,12 @@ class OCR(ctk.CTkFrame):
         self.label = ctk.CTkLabel(self, text = self.data)
         self.label.pack()
 
-    def add(self):
         
-        self.gobackBtn = ctk.CTkButton(self, text = "Go Back", command = self.goback)
-        self.gobackBtn.pack()
+
+    def add(self):
+        self.topic = self.enterTopic.get()
+        ref.child(f'{self.currentEmail}').update({f'{self.topic}': f'{self.data}'})
+        
 
    
 
@@ -259,31 +269,34 @@ class OCR(ctk.CTkFrame):
 class LoginFrame(ctk.CTkFrame):
     def __init__(self, container):
         super().__init__(container)
+
+         
+        
         self.container = container
         self.emailEntry = ctk.CTkEntry(master=self, placeholder_text="Email.")
-        self.emailEntry.pack(padx=30, pady=20)
+        self.emailEntry.pack(padx=30, pady=10)
         self.passwordEntry = ctk.CTkEntry(master=self, show="*", placeholder_text="Password.")
-        self.passwordEntry.pack(padx=30, pady=20)
+        self.passwordEntry.pack(padx=30, pady=10)
         self.nameEntry = ctk.CTkEntry(master=self, placeholder_text="Name.")
-        self.nameEntry.pack(padx=30, pady=20)
+        self.nameEntry.pack(padx=30, pady=10)
         self.ageEntry = ctk.CTkEntry(master=self, placeholder_text="Age.")
-        self.ageEntry.pack(padx=30, pady=20)
+        self.ageEntry.pack(padx=30, pady=10)
         self.heightEntry = ctk.CTkEntry(master=self, placeholder_text="Height (in cm).")
-        self.heightEntry.pack(padx=30, pady=20)
+        self.heightEntry.pack(padx=30, pady=10)
         self.bloodEntry = ctk.CTkEntry(master=self, placeholder_text="Blood type.")
-        self.bloodEntry.pack(padx=30, pady=20)
+        self.bloodEntry.pack(padx=30, pady=10)
         self.allergyEntry = ctk.CTkEntry(master=self, placeholder_text="Allergies.")
-        self.allergyEntry.pack(padx=30, pady=20)
+        self.allergyEntry.pack(padx=30, pady=10)
         self.phobiaEntry = ctk.CTkEntry(master=self, placeholder_text="Phobias.")
-        self.phobiaEntry.pack(padx=30, pady=20)
+        self.phobiaEntry.pack(padx=30, pady=10)
         self.vaxEntry = ctk.CTkEntry(master=self, placeholder_text="Prior vaccinations.")
-        self.vaxEntry.pack(padx=30, pady=20)
+        self.vaxEntry.pack(padx=30, pady=10)
         self.weightEntry = ctk.CTkEntry(master=self, placeholder_text="Weight (in kg).")
-        self.weightEntry.pack(padx=30, pady=20)
+        self.weightEntry.pack(padx=30, pady=10)
         self.pfp = ctk.CTkButton(self, text = "Upload picture of yourself.", command = self.uploadpfp)
-        self.pfp.pack(padx=30, pady=20)
+        self.pfp.pack(padx=30, pady=10)
         self.button = ctk.CTkButton(master=self, text="Sign Up.", command=self.button_function_two)
-        self.button.pack(padx=30, pady=20)  
+        self.button.pack(padx=30, pady=10)  
     def uploadpfp(self):
         self.filename = filedialog.askopenfilename(initialdir = "D:\\python grind\\oakridge hackathon", title = "select a file", filetypes = (("png files", "*.png"),("jpg files", "*.jpg")))
     def button_function_two(self):
