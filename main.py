3c1b96c57f027d1ad76d5e6e0a6e4cf0bc42592e
Kushal <74308033+kool123r5@users.noreply.github.com>
2023-01-21 14:42:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ cred = credentials.Certificate('/Users/kushalb/Documents/VSCode/OakridgeHacks202
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://oakridgecodefest2023-default-rtdb.firebaseio.com/'
 })
-currentEmail = ""
+
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 ref = db.reference('/')
-ref.child('John Deer').set({
-    'Image': 'no img right now',
-    'Name': 'Ashwin Ganapathy',
-    'Age': 16,
-    'Blood-type': 'B+',
-    'Height': '165cm',
-    'Allergies': 'Nuts',
-    'Phobias': 'Arachnophobia',
-    'Prior Vaccinations': 'COVID-19'
-})
+# ref.child('John Deer').set({
+#     'Image': 'no img right now',
+#     'Name': 'Ashwin Ganapathy',
+#     'Age': 16,
+#     'Blood-type': 'B+',
+#     'Height': '165cm',
+#     'Allergies': 'Nuts',
+#     'Phobias': 'Arachnophobia',
+#     'Prior Vaccinations': 'COVID-19'
+# })
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
@@ -35,8 +35,17 @@ def button_function():
 
 def button_function_two():
     auth.create_user(email=emailEntry.get(), password=passwordEntry.get())
-    currentEmail = emailEntry.get()
     frame_2.tkraise()
+    ref.child(f'{emailEntry.get().replace("@", "").replace(".", "")}_{nameEntry.get()}').set({
+        'Image': 'no img right now',
+        'Name': f'{nameEntry.get()}',
+        'Age': f'{ageEntry.get()}',
+        'Blood-type': f'{bloodEntry.get()}',
+        'Height': f'{heightEntry.get()}',
+        'Allergies': f'{allergyEntry.get()}',
+        'Phobias': f'{phobiaEntry.get()}',
+        'Prior Vaccinations': f'{vaxEntry.get()}'
+    })
 
 # Use CTkButton instead of tkinter Button
 frame_2 = customtkinter.CTkFrame(master=app)
@@ -44,11 +53,25 @@ frame_2.place(relx=0.5, rely=0.5, anchor='center')
 frame_1 = customtkinter.CTkFrame(master=app)
 frame_1.place(relx=0.5, rely=0.5, anchor='center')
 emailEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Email.")
-emailEntry.pack(padx=20, pady=20)
+emailEntry.pack(padx=30, pady=20)
 passwordEntry = customtkinter.CTkEntry(master=frame_1, show="*", placeholder_text="Password.")
-passwordEntry.pack(padx=20, pady=20)
+passwordEntry.pack(padx=30, pady=20)
+nameEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Name.")
+nameEntry.pack(padx=30, pady=20)
+ageEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Age.")
+ageEntry.pack(padx=30, pady=20)
+heightEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Height (in cm).")
+heightEntry.pack(padx=30, pady=20)
+bloodEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Blood type.")
+bloodEntry.pack(padx=30, pady=20)
+allergyEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Allergies.")
+allergyEntry.pack(padx=30, pady=20)
+phobiaEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Phobias.")
+phobiaEntry.pack(padx=30, pady=20)
+vaxEntry = customtkinter.CTkEntry(master=frame_1, placeholder_text="Prior vaccinations.")
+vaxEntry.pack(padx=30, pady=20)
 button = customtkinter.CTkButton(master=frame_1, text="Sign Up.", command=button_function_two)
-button.pack(padx=20, pady=20)
+button.pack(padx=30, pady=20)
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=frame_2, text="CTkButton", command=button_function)
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
