bbceac2d53d381430c7b6d15a5df31404f9bbe8d
shwinn08 <81485314+shwinn08@users.noreply.github.com>
2023-01-22 00:44:05 +0530

    Update test.js

--- calendar/test.js
+++ calendar/test.js
@@ -788,8 +788,14 @@ mql.addListener(function(m) {
 	}
 });
 
-//things i regret about this project:
-//1- i didnt use a design pattern !
-//2- i used date object as a string instead of date formate in notes
-//3- as the feauters progress i end up with a spagheti code ! sorry :(
-// FACT: it wouldn't be possible without the builtin date object "new Date()" thanks javascript !
+
+const alert = document.getElementById('alert')
+alert.addEventListener(`click`, function(){
+	let elephant = document.querySelector("td");
+    elephant.style.backgroundColor = "red";
+    console.log("works")
+	
+	
+})
+
+
